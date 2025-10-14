77255eed8afac1bb9c937e1067c729facec7a4fb
Alex Freidah <alex.freidah@gmail.com>
2025-10-13 18:14:22 -0700

    hooking into dbus and returning 200 for health service now

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/afreidah/health-check-service
 
 go 1.25.1
+
+require github.com/coreos/go-systemd/v22 v22.6.0
+
+require github.com/godbus/dbus/v5 v5.1.0 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/coreos/go-systemd/v22/dbus"
 )
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, "OK")
+func healthHandler(w http.ResponseWriter, r *http.Request, conn *dbus.Conn, service string) {
+	prop, err := conn.GetUnitPropertyContext(r.Context(), service+".service", "ActiveState")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	activeStatus := prop.Value.Value().(string)
+
+	if activeStatus == "active" {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusServiceUnavailable)
+	}
+	log.Printf("Status %s", activeStatus)
 }
 
 func main() {
@@ -21,7 +35,17 @@ func main() {
 	port := *portFlag
 	service := *serviceFlag
 
-	http.HandleFunc("/health", healthHandler)
+	// dbus connection
+	ctx := context.Background()
+	conn, err := dbus.NewSystemConnectionContext(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		healthHandler(w, r, conn, service)
+	})
 
 	log.Printf("Monitoring %s on :%d", service, port)
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
